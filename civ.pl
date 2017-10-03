7b100c3ad52416d6616bee4f59e0c07224e36135
fockjef <fockjef@baroness-blood.dreamhost.com>
2017-10-03 11:24:29 -0700

    Tweak help message

--- civ.pl
+++ civ.pl
@@ -21,8 +21,8 @@ Usage:
 	civ [-dg -c <N> -h <N> -h <N>] <file>
 
 Options:
-	-d   Dither image
-	-g   Greyscale image
+	-d       Dither image
+	-g       Greyscale image
 	-c <N>   Use N colors for output (typically 8,16, or 256)
 	-w <N>   Print at most N columns of output
 	-h <N>   Print at most N rows of output
